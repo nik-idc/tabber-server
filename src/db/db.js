18a732f98c2f6dc1f69f1448c6ff0799abab0c55
tikina <nikvmain@gmail.com>
2023-05-18 11:02:28 +0300

    Changed gitignore to ignore sequelize db config

--- src/db/db.js
+++
@@ -1,9 +0,0 @@
-// const Pool = require('pg').Pool;
-// const pool = new Pool({
-// 	user: 'postgres',
-// 	password: 'nik_pg15',
-// 	port: 5432,
-// 	database: 'tabber_db'
-// });
-
-// module.exports = pool;
